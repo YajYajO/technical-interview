037bc64f79a99ba8f03014556438a8bd127abff0
Jon-Joel Ocfemia <60579992+YajYajO@users.noreply.github.com>
2023-10-19 16:02:29 -0400

    Add files via upload

---
+++ Folder 1- Static Website/Assets.js
@@ -0,0 +1,5 @@
+export default function Assets(){
+    return(
+        <img src="C:\Users\Jj_oc\OneDrive\Documents\College Files\Self Projects\ReactWarmUps\nagimo-technical\src\ClimberPic.PNG"/>
+    )
+}
